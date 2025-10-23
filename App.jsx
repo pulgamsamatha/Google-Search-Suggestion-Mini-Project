850bb59780451dfe7b42329b25e5ac0c22a53266
Samatha Pulgam <pulgamsamatha@gmail.com>
2025-10-23 14:52:29 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import './App.css'
+import GoogleSuggestions from "./Components/GoogleSuggestions"
+
+const suggestionsList = [
+  {id: 1, suggestion: 'Price of Ethereum'},
+  {id: 2, suggestion: 'Oculus Quest 2 specs'},
+  {id: 3, suggestion: 'Tesla Share Price'},
+  {id: 4, suggestion: 'Price of Ethereum today'},
+  {id: 5, suggestion: 'Latest trends in AI'},
+  {id: 6, suggestion: 'Latest trends in ML'},
+]
+
+
+const App = () => {
+  return (
+    <div>
+      <GoogleSuggestions suggestionsList = {suggestionsList}/>
+    </div>
+  );
+};
+
+export default App;
